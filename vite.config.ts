583ab72294504c985653aaa29325c525d2412d66
vatsalgargg <vatsalg80@gmail.com>
2026-04-30 20:48:13 +0530

    fix: remove duplicate vite export, add react deps, add .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/mad-frontend-app/', // Must match your GitHub repository name exactly!
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
@@ -33,9 +34,4 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
-})
-
-export default defineConfig({
-  base: '/mad-frontend-app/', // Must match your GitHub repository name exactly!
-  plugins: [react()],
 })
